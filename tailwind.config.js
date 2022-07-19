7a46cd71a2672ba3a0b7b3d90857e82a8c5faf3c
T-oluwadara <ajimotioluwadara@gmail.com>
2022-07-19 05:08:02 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        paleWhite: 'hsl(0, 0%, 98%)',
+        lightGreenish: 'hsla(159, 62%, 89%)'
+      },
+    },
+  },
+  plugins: [],
+}
